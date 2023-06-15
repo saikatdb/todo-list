fcad7b77079da9559cd461c861863bed56faa738
Saikat Debnath <112309999+saikatdb@users.noreply.github>
2023-06-15 12:11:42 +0530

    Add css and image asset management module

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   devtool: 'inline-source-map',
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
